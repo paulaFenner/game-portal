bc32108a5d023ec35c3d51854b40763e0f4ec1ea
Paula Fenner <fenner.paula@gmail.com>
2025-04-01 16:41:18 +0200

    add modal and functionality to buttons

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,35 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function Player() {
-  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
-  const [submitted, setSubmitted] = useState(false);
+  const playerName = useRef();
 
-  function handleChange(e) {
-    setEnteredPlayerName(e.target.value);
-  }
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  // since we're using refs now, we don't have to update the state on each change
+  // const [submitted, setSubmitted] = useState(false);
+
+  //   function handleChange(e) {
+  //     setEnteredPlayerName(e.target.value);
+  //   }
 
   function handleClick() {
-    setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = ''; /* clears the input field 
+    - not optimal when using React as directly changing the DOM */
   }
 
   return (
     <section id='player'>
-      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+      <h2>
+        Welcome {enteredPlayerName ? enteredPlayerName : 'unknown entity'}
+      </h2>
       <p>
         <input
+          ref={playerName}
           type='text'
-          onChange={handleChange}
-          value={enteredPlayerName}
+          // the input now is linked to the ref, so we don't need the value anymore
+          // onChange={handleChange}
+          //   value={enteredPlayerName}
         />
         <button onClick={handleClick}>Set Name</button>
       </p>
